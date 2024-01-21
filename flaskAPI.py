82c4281a26f767840616c7100ecaf74fb6fe968a
yogiear <yogieariana.ftmd18@gmail.com>
2024-01-21 22:26:55 +0700

    Trying POST API using postman software.

--- flaskAPI.py
+++ flaskAPI.py
@@ -16,5 +16,11 @@ def get_user(user_id):
 
   return jsonify(user_data), 200
 
+@app.route("/create-user", methods=["POST"])
+def create_user():
+  data = request.get_json()
+
+  return jsonify(data), 201
+
 if __name__ == "__main__":
   app.run(debug=True)
